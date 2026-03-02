0da8e5a3c6bff3c4847f2dabc21b5abd56c5f69a
Ryan Curran <rcurran@mozilla.com>
2026-03-02 11:10:49 -0500

    Initial Commit

---
+++ BuildWatch/BuildWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildWatchApp.swift
+//  BuildWatch
+//
+//  Created by Ryan Curran on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BuildWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
